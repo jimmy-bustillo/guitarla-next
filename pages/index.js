8b1a505f2f6cbdf2fdcc1e2a09df4059e53b665d
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-29 21:21:11 -0600

    Añade los meta dinamicos para el SEO

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,10 @@ import Layout from "../components/layout"
 export default function Home() {
   return (
     <>
-      <Layout>
+      <Layout
+        title={'Inicio'}
+        description={'Blog de Musica, venta de guitarras y mas'}
+      >
         <h1>Hola mundo desde Next</h1>
         <Link href='/nosotros'>Ir a Nosotros</Link>
       </Layout>
