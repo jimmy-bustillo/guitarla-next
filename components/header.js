2a01a6da89e4c534dbe23118fc03c7d87e3b7483
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-30 21:33:04 -0600

    Importa los estilos con css modules

--- components/header.js
+++ components/header.js
@@ -1,17 +1,23 @@
 import Image from "next/image"
 import Link from "next/link"
+import styles from "../styles/header.module.css"
 
 export default function Header() {
   return (
-    <header>
-      <div className='contenedor'>
-        <Image src="/img/logo.svg" width={300} height={40} alt='imagen logotipo' />
-        <nav>
+    <header className={styles.header}>
+      <div className={`contenedor ${styles.barra}`}>
+        <Image
+          src='/img/logo.svg'
+          width={300}
+          height={40}
+          alt='imagen logotipo'
+        />
+
+        <nav className={styles.navegacion}>
           <Link href='/'>Inicio</Link>
           <Link href='/nosotros'>Nosotros</Link>
           <Link href='/blog'>Blog</Link>
           <Link href='/tienda'>Tienda</Link>
-
         </nav>
       </div>
     </header>
