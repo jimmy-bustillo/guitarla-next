d556dac54bd4b1501e0c779614c3324bc3f11105
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-30 21:12:36 -0600

    Agrega el layout de tienda

---
+++ pages/tienda.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout"
+
+export default function Tienda() {
+  return (
+    <Layout
+      title={"Tienda Virtual"}
+      description='Tienda virtual, venta de guitarras, intrumentos, GuitarLA, tienda de música'
+    >
+      <h1>Tienda</h1>
+    </Layout>
+  )
+}
