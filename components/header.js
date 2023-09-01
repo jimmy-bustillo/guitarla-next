6b5067f601fe234db7b78da970f8cd8e2b942703
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-08-31 21:21:31 -0600

    Utiliza useRouter para resaltar la pagina actual

--- components/header.js
+++ components/header.js
@@ -1,23 +1,48 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import styles from "../styles/header.module.css"
 
 export default function Header() {
+  const router = useRouter()
+
   return (
     <header className={styles.header}>
       <div className={`contenedor ${styles.barra}`}>
-        <Image
-          src='/img/logo.svg'
-          width={300}
-          height={40}
-          alt='imagen logotipo'
-        />
+        <Link href={'/'}>
+          <Image
+            src='/img/logo.svg'
+            width={300}
+            height={40}
+            alt='imagen logotipo'
+          />
+        </Link>
 
         <nav className={styles.navegacion}>
-          <Link href='/'>Inicio</Link>
-          <Link href='/nosotros'>Nosotros</Link>
-          <Link href='/blog'>Blog</Link>
-          <Link href='/tienda'>Tienda</Link>
+          <Link
+            className={router.pathname === "/" ? styles.active : ""}
+            href='/'
+          >
+            Inicio
+          </Link>
+          <Link
+            className={router.pathname === "/nosotros" ? styles.active : ""}
+            href='/nosotros'
+          >
+            Nosotros
+          </Link>
+          <Link
+            className={router.pathname === "/blog" ? styles.active : ""}
+            href='/blog'
+          >
+            Blog
+          </Link>
+          <Link
+            className={router.pathname === "/tienda" ? styles.active : ""}
+            href='/tienda'
+          >
+            Tienda
+          </Link>
         </nav>
       </div>
     </header>
