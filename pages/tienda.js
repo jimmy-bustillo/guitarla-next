bba99c8d12211c722275fadd9af9dd88da9e40e9
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-10-10 21:22:10 -0600

    Importa los estilos de grid para la tienda

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout"
 import Guitarra from "../components/guitarra"
+import styles from "../styles/grid.module.css"
 
 export default function Tienda({ guitarras }) {
   return (
@@ -9,12 +10,12 @@ export default function Tienda({ guitarras }) {
     >
       <main className='contenedor'>
         <h1 className='heading'>Nustra Colección</h1>
-        {guitarras.map((guitarra) => (
-          <Guitarra 
-            key={guitarra.id}
-            guitarra={guitarra.attributes}
-          />
-        ))}
+
+        <div className={styles.grid}>
+          {guitarras.map((guitarra) => (
+            <Guitarra key={guitarra.id} guitarra={guitarra.attributes} />
+          ))}
+        </div>
       </main>
     </Layout>
   )
