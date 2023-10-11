a15d8de7e283fc48b0bb93fbe0b13fdfee1ad8f2
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-10-10 21:21:52 -0600

    Importa los estilos para las guitarras

--- components/guitarra.js
+++ components/guitarra.js
@@ -1,11 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
+import styles from "../styles/guitarras.module.css"
 
 export default function Guitarra({ guitarra }) {
   const { descripcion, imagen, nombre, precio, url } = guitarra
 
   return (
-    <div>
+    <div className={styles.guitarra}>
       <Image
         src={imagen.data.attributes.formats.medium.url}
         width={600}
@@ -13,11 +14,11 @@ export default function Guitarra({ guitarra }) {
         alt={`Imagen guitarra ${nombre}`}
       />
 
-      <div>
+      <div className={styles.contenido}>
         <h3>{nombre}</h3>
-        <p>{descripcion}</p>
-        <p>${precio}</p>
-        <Link href={`/guitarras/${url}`}>Ver Producto</Link>
+        <p className={styles.descripcion}>{descripcion}</p>
+        <p className={styles.precio}>${precio}</p>
+        <Link className={styles.enlace} href={`/guitarras/${url}`}>Ver Producto</Link>
       </div>
     </div>
   )
