53d3715635bd9407fccf54feb9fda388a2283936
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2023-10-27 21:45:21 -0600

    Arregla caracter en la ruta de los blogs

--- components/post.js
+++ components/post.js
@@ -19,7 +19,7 @@ export default function Post({ post }) {
         <h3>{titulo}</h3>
         <p className={styles.fecha}>{formatearFecha(publishedAt)}</p>
         <p className={styles.resumen}>{contenido}</p>
-        <Link className={styles.enlace} href={`/nlog/${url}`}>
+        <Link className={styles.enlace} href={`/blog/${url}`}>
           Leer Post
         </Link>
       </div>
